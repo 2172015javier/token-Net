ea81bb022519fa9b13623e798087308f8684da47
javier <narcisista12@gmail.com>
2024-09-02 17:13:26 -0500

    first commit

---
+++ WebAPI/Controllers/ProductoController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Custom;
+using WebAPI.Models;
+using WebAPI.Models.DTOs;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize]
+    [ApiController]
+    public class ProductoController : ControllerBase
+    {
+        private readonly DbpruebaContext _dbpruebaContext;
+
+        public ProductoController(DbpruebaContext)
+        {
+            
+        }
+    }
+}
---
+++ WebAPI/Custom/Utilidades.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Runtime.Intrinsics.Arm;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using WebAPI.Models;
+
+namespace WebAPI.Custom
+
+{
+    public class Utilidades
+    {
+        private readonly IConfiguration _configuration;
+        public Utilidades(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+        }
+        public string encriptarSHA256(string texto)
+        {
+            using (SHA256 sha256HASH = SHA256.Create()) {
+                //computar el hash
+                byte[] bytes = sha256HASH.ComputeHash(Encoding.UTF8.GetBytes(texto));
+
+                //convertir el array en bytes a string
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+        public String generarJWT(Usuario modelo)
+        {
+            //crear la informacion del Usuario para el token
+            var userClaims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, modelo.IdUsuario.ToString()),
+                new Claim(ClaimTypes.Email, modelo.Correo!)
+
+            };
+            var securitykey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:key"]!));
+            var credentials = new SigningCredentials(securitykey, SecurityAlgorithms.HmacSha256Signature);
+
+            // crear el detalle del token
+
+            var jwtConfig = new JwtSecurityToken(
+                claims: userClaims,
+                expires: DateTime.UtcNow.AddMinutes(20),
+                signingCredentials: credentials
+                );
+            return new JwtSecurityTokenHandler().WriteToken(jwtConfig);
+        }
+    }
+}
